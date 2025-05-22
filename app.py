7f6c95e60757b6f01e928ede66f4fc1fda54dcec
magwrap <jasiek.musiol@gmail.com>
2025-05-22 16:05:28 +0200

    chore: add todo

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import uvicorn
 
+# TODO: unitify the environment so that client, scheduler and worker are using the same one
 
 if __name__ == "__main__":
     uvicorn.run("src.main:app", host="127.0.0.1", port=6969, reload=True)
